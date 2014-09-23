10c6c2a7a926817f3c44a4a370df2169574c2b59
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-09-23 16:11:30 -0400

    Put in the service request to load the file

--- angular/assets/js/app/main-app.start.js
+++ angular/assets/js/app/main-app.start.js
@@ -5,35 +5,34 @@ angular.module('MainApp',[
 
 ])
 
-.controller("MainController",function($scope){
-
-	$scope.categories =[
-		 {"name":"Dave Voyles","location":"Philadelphia"},
-         {"name":"Stacey Mulcahey","location":"New York"},
-         {"name":"Jason Fox","location":"Dallas"}
-         ]
-  $scope._name = "stacey";
-  $scope._location = "nyc";
+.controller("MainController",function($scope,$http){
+ $http.get('assets/data/evangelists.json')
+       .then(function(res){
+          $scope.categories = res.data.evangelist;                
+        });
+  
+  $scope._name = "Default Name";
+  $scope._location = "Default Location";
   $scope.user ={
-  	name:function(theName){
-  		if(angular.isDefined(theName))
-  		{
-  			this._name = theName;
-  		}
+    name:function(theName){
+      if(angular.isDefined(theName))
+      {
+        this._name = theName;
+      }
 
-  		return this._name;
-  		
-  	},
-  	location:function(theLocation){
-  		if(angular.isDefined(theLocation)){
-  			this._location = theLocation;
-  		}
+      return this._name;
+      
+    },
+    location:function(theLocation){
+      if(angular.isDefined(theLocation)){
+        this._location = theLocation;
+      }
       return this._location;
-  	}
+    }
   }
 
  function addName(user){
- 	$scope.categories.push(user);
+  $scope.categories.push(user);
  }
 
  $scope.addName = addName;
